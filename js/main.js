05e65224cc06a5ef235d9c6e716743cb510b3172
Dmitry <42784166+dee1992@users.noreply.github.com>
2021-08-20 22:35:13 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 $(document).ready(function () {
-  var currentFloor = 2;
-  var floorPath = $('.home-image path');
-  var counterUp = $('.counter-up');
-  var counterDown = $('.counter-down');
+  var currentFloor = 2; // текущий этаж
+  var floorPath = $('.home-image path'); // каждый отдельный этаж - svg файл
+  var counterUp = $('.counter-up'); // кнопка увеличения 
+  var counterDown = $('.counter-down'); // кнопка уменьшения
+  var modal = $('.modal');
+  var modalCloseButton = $('.modal-close-button');
+  var viewFlatsButton = $('.view-flats');
+
+  // функция при наведени мышью на этаж
   floorPath.on('mouseover', function () {
      floorPath.removeClass('current-floor');
     currentFloor = $(this).attr('data-floor');
     $('.counter').text(currentFloor);
   });
+
+  floorPath.on('click', toggleModal); // отдельно записал функцию открывающее модальное окно
+  modalCloseButton.on('click', toggleModal); // отдельно записал функцию открывающее модальное окно
+  viewFlatsButton.on('click', toggleModal);
 
 counterUp.on('click', function () {
 if (currentFloor < 18) {
@@ -29,5 +38,8 @@ if (currentFloor < 18) {
       floorPath.removeClass('current-floor');
   $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
     }
-  })
+  });
+  function toggleModal() { // функция открыть/закрыть окно
+    modal.toggleClass('is-open');
+  }
 }); 
